2b52dc23af9e0db1de604b5189f590da6e2030d6
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-10-16 15:44:13 +0200

    Refactor MessageController to return a list of messages

--- backend/src/main/java/com/auth/jwt/backend/messages/MessageController.java
+++ backend/src/main/java/com/auth/jwt/backend/messages/MessageController.java
@@ -1,15 +1,25 @@
 package com.auth.jwt.backend.messages;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Arrays;
+import java.util.List;
 
-@Controller
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/v1")
+@CrossOrigin(origins = "http://localhost:4200") // Allow cross-origin requests from Angular
 public class MessageController {
     
     @GetMapping("/messages")
-    public ResponseEntity<String> getMessages() {
-        return new ResponseEntity<>("Hello, World!", org.springframework.http.HttpStatus.OK);
+    public ResponseEntity<List<String>> getMessages() {
+        return ResponseEntity.ok(Arrays.asList("eins", "zwei", "Polizei"));
     }
 
 }

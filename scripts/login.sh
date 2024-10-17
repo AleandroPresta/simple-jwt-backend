9c7ad811cd08b972bc5062bd1a46404d723d5ff3
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-10-17 10:33:30 +0200

    Add login and register scripts

---
+++ scripts/login.sh
@@ -0,0 +1,6 @@
+curl -X POST http://localhost:8080/api/v1/login \
+-H "Content-Type: application/json" \
+-d '{
+  "login": "aleandro",
+  "password": "admin"
+}'
---
+++ scripts/register.sh
@@ -0,0 +1,8 @@
+curl -X POST http://localhost:8080/api/v1/register \
+-H "Content-Type: application/json" \
+-d '{
+  "firstName": "aleandro",
+  "lastName": "presta",
+  "login": "aleandro",
+  "password": "admin"
+}'
